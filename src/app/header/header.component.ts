7872224bdc609e15266de91c86f401168ace704b
masongco <markallensongco@gmail.com>
2025-05-17 09:33:12 +0800

    added mobile response, fixed styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,9 +10,22 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
+
   constructor(public auth: AuthService) {}
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : '';
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+
   logout(): void {
+    this.closeMenu();
     this.auth.logout();
   }
 }
